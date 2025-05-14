dabe2c679979691a36faee48ced6e7a5aebe4314
Jyoshitha Pechetti <jyoshithapechetti03@gmail.com>
2025-05-14 19:38:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_L3S0ZWcXv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3cikq05p80946vmhn2apdpn6l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_t9lepNRWw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6seeega77kvk73i2j0t64kv1m1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://psh6qaosmg.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://kdj0coc5n4.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
